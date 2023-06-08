1815ab94b43f7afbbce39bec871640ffdc99d0b3
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2023-06-08 19:58:40 -0400

    index page created

--- user/src/main.jsx
+++ user/src/main.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+     <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import {BrowserRouter, BrowserRouter as Router} from "react-router-dom"
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <Router>
+//     <BrowserRouter>
+//     <App />
+//     </BrowserRouter>
+//   </Router>
+// );
