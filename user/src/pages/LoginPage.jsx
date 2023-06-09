c410e392fcc4d5c299885a03a9f13bc32b6baa09
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2023-06-08 22:41:11 -0400

    css for login

--- user/src/pages/LoginPage.jsx
+++ user/src/pages/LoginPage.jsx
@@ -2,8 +2,13 @@ import Header from "../Header"
 
 function LoginPage(){
     return(
-<div>
-   login page
+<div className="mt-8">
+    <h1 className="text-center text-6xl font-bold">Login</h1>
+   <form className="max-w-nd mx-auto border">
+    <input type="username/email" placeholder="username/email" />
+    <input type="password" placeholder="password" />
+    <button className="primary">Login</button>
+   </form>
 </div>
     )
 }
