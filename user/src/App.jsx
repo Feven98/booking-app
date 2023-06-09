2f504ac5dd7d94d8ac38997387294c2683b96bc8
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2023-06-08 20:08:28 -0400

    add route for login

--- user/src/App.jsx
+++ user/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import IndexPage from './files/IndexPage'
+import IndexPage from './pages/IndexPage'
+import LoginPage from './pages/LogInPage'
 
 function App() {
 
   return (
     <Routes>
-      <Route index element={<IndexPage />} />
+      <Route path='/' element={<IndexPage />} />
+      <Route path='/login' element={<LoginPage/>} />
     </Routes>
    
   )
--- user/src/main.jsx
+++ user/src/main.jsx
@@ -11,17 +11,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>,
 )
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import {BrowserRouter, BrowserRouter as Router} from "react-router-dom"
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <Router>
-//     <BrowserRouter>
-//     <App />
-//     </BrowserRouter>
-//   </Router>
-// );
