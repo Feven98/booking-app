6ed1d8037ef3adc706c643679eb16f80d0920c8b
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2023-06-08 23:35:39 -0400

    install express and create index

--- user/src/pages/SignupPage..jsx
+++ user/src/pages/SignupPage..jsx
@@ -1,15 +1,23 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
 function SignupPage(){
+    const [username,setUserName] = useState('');
+    const [email,setEmail] = useState('');
+    const [newpassword,setNewPassword] = useState('');
+    const [password,setPassword]= useState('');
+    function register() {
+        
+    }
     return(
         <div className="mt-10 grow flex items-center justify-around">
     <div className="mb-12">
     <h1 className="text-center text-6xl font-bold mb-6">Sign Up</h1>
-   <form className="max-w-md mx-auto border">
-    <input type="username" placeholder="username"/>
-    <input type="email" placeholder="email"/>
-    <input type="new password" placeholder="new password" />
-    <input type="password" placeholder="password" />
+   <form className="max-w-md mx-auto border" onSubmit={register}>
+    <input type="username" placeholder="username" value={username} onChange={ev=> setUserName(ev.target.value)}/>
+    <input type="email" placeholder="email" value={email} onChange={ev=> setEmail(ev.target.value)}/>
+    <input type="new password" placeholder="new password" value={newpassword} onChange={ev=> setNewPassword(ev.target.value)} />
+    <input type="password" placeholder="password" value={password} onChange={ev=> setPassword(ev.target.value)} />
     <button className="primary">Sign Up</button>
     <div className="text-center py-4">
         If you do have an account, click on 
@@ -19,6 +27,7 @@ function SignupPage(){
 </div>
 </div>
     )
+    
 }
 
 export default SignupPage
