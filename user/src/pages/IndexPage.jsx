eeb90040a37fcf9b2c47d630cc0e8fcdd3913632
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2023-06-08 20:23:51 -0400

    create header file for all headers

--- user/src/pages/IndexPage.jsx
+++ user/src/pages/IndexPage.jsx
@@ -1,39 +1,10 @@
+import { Link } from "react-router-dom"
+import Header from "../Header"
+
 function IndexPage(){
     return(
     <div>
-    <header className='p-3 flex justify-between'>
-      <a href='' className='logo'>
-      {/* the person do =  */}
-      {/* <a href='' className='flex items-center gap-1'> */}
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-10 h-10">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 010 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 010-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375z" />
-</svg>
-<span className='font-bold'>MAFND</span>
-</a>
-<div className='flex gap-3 border border-violet-700 rounded-lg py-4 px-6 shadow-md shadow-violote-900'>
-  <div>Place</div>
-  <div className="border-l border-violet-800"></div>
-  <div>Date</div>
-  <div className="border-l border-violet-800"></div>
-  <div>Person</div>
-  <button className='bg-primary text-white p-1.5 rounded-full'>
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 15.75l-2.489-2.489m0 0a3.375 3.375 0 10-4.773-4.773 3.375 3.375 0 004.774 4.774zM21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-</svg>
-  </button>
-</div>
-<div className='flex item-center gap-3 border border-violet-700 rounded-lg py-4 px-6'>
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-</svg>
-<div className='bg-primary text-white rounded-full p-2'>
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-  <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
-</svg>
-
-</div>
-</div>
-    </header>
+    <Header />
    </div>
     )
 }
