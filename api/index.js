f6f4592f55b6c60586ad5c03a7a6f04233934aa1
Feven Seifu <rutemikey1@gmail.com>
2023-06-13 23:18:02 -0400

    connect to mongo db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
+// initialize .env variables
+require("dotenv").config();
+
+// pull PORT from .env, give default value of 4000 and establish DB Connection
+
+
+const { PORT, MONGODB_URI } = process.env;
+
+
 const express = require('express');
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
 const app = express();
 
 app.use(express.json());
@@ -8,9 +18,12 @@ app.use(cors({
     origin: 'http://localhost:5173',
 }));
 
+
 app.get('/test', (req,res) => {
     res.json('test ok');
 });
+
+// XZgDPptjfX7eh9Fl
 
 app.post('/signup', (req,res) => {
     const {username,email,newpassword,password} = req.body;
