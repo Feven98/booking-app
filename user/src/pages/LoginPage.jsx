c2d42c9ceb59e48a744f7840f2a80d97064075db
Feven Seifu <rutemikey1@gmail.com>
2023-06-15 18:53:39 -0400

    cleaning some code

--- user/src/pages/LoginPage.jsx
+++ user/src/pages/LoginPage.jsx
@@ -8,7 +8,6 @@ function LoginPage(){
     <h1 className="text-center text-6xl font-bold mb-6">Login</h1>
    <form className="max-w-md mx-auto border">
     <input type="username" placeholder="username" />
-    <input type="password" placeholder="password" />
     <button className="primary">Login</button>
     <div className="text-center py-4">
         If you don't have an account, click on 
