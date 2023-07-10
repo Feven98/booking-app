fe347984a6f841558924a9dbff5dcd99c6720c84
Feven Seifu <rutemikey1@gmail.com>
2023-07-09 23:56:13 -0400

    make comment out

--- user/src/Header.jsx
+++ user/src/Header.jsx
@@ -15,8 +15,8 @@ function Header() {
                 <div>Place</div>
                 <div className="border-l border-violet-800"></div>
                 <div>Date</div>
-                <div className="border-l border-violet-800"></div>
-                <div>Person</div>
+                {/* <div className="border-l border-violet-800"></div>
+                <div>Person</div> */}
                 <button className='bg-primary text-white p-1.5 rounded-full'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 15.75l-2.489-2.489m0 0a3.375 3.375 0 10-4.773-4.773 3.375 3.375 0 004.774 4.774zM21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
