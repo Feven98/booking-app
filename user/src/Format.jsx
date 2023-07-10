772cba8c040a0d416a6c55547625a88aa4fb6bd2
Feven Seifu <rutemikey1@gmail.com>
2023-07-09 23:48:14 -0400

    adding header to format

--- user/src/Format.jsx
+++ user/src/Format.jsx
@@ -4,7 +4,7 @@ import Header from "./Header"
 function Format() {
     return (
         <div className="p-5 flex flex-col min-h-screen">
-          
+           <Header />
            <Outlet />
         </div>
     )
